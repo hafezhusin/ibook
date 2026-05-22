5dafe78c872ec1288eaf13dbdadea4cbf7d4904c
Mohd Hafez Bin Husin <hafez@anm.gov.my>
2026-05-22 08:32:54 +0800

    security: block sensitive dirs via .htaccess, obfuscate /up health endpoint

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: '/system-health-'.substr(md5(env('APP_KEY', 'ibook')), 0, 8),
     )
     ->withMiddleware(function (Middleware $middleware) {
         // Daftarkan alias middleware
